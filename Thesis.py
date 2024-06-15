a4f759d7ff58ec56620b0d2236d1b462d797c4bb
Malassalive <138037131+Malassalive@users.noreply.github.com>
2024-06-15 17:50:03 +0300

    Update Thesis.py

--- Thesis.py
+++ Thesis.py
@@ -2,42 +2,42 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Удобный инструмент для планирования трудоемкости')
+st.title('A convenient tool for planning labor costs')
 st.image("https://lh3.google.com/u/0/d/1N_hl7gRTc_IBeoVc0lbgnXzsbkoNtMX1=w1318-h644-iv1")
-st.header('Данная версия является ознакомительной и больше похожа на генератор случайных чисел')
+st.header('This version is only a demonostrate, the results are not reliaable')
 st.divider()
-st.sidebar.header('Данные об исполнителе')
-experience = st.sidebar.number_input('Стаж в годах',0,100)
-department = st.sidebar.selectbox('Отдел',['АСО','ВиВ','ОВиК'])
-building = st.sidebar.selectbox("Сооружение",["Аэротенк","Камера сбора осадка","Буферный резервуар","Здание решеток","Воздуходувная станция","Отстойник","Резервуар-усреднитель","НСВИ","Цех обработки осадка","АБК","Мехочистка","Реагентное хозяйство ФК"])
+st.sidebar.header('Information about the employee')
+experience = st.sidebar.number_input('Working experince',0,100)
+department = st.sidebar.selectbox('Department',['АСО','ВиВ','ОВиК'])
+building = st.sidebar.selectbox("Projecting building",["Аэротенк","Камера сбора осадка","Буферный резервуар","Здание решеток","Воздуходувная станция","Отстойник","Резервуар-усреднитель","НСВИ","Цех обработки осадка","АБК","Мехочистка","Реагентное хозяйство ФК"])
 
-st.header('Информация о проектируемом разделе')
-st.text('Введите основные вводные данные для расчета трудоемкости')
+st.header('Projectiong data')
+st.text('Inputs for the calculations of labor costs')
 col1,col2, col3, col4 = st.columns(4)
-df1 = pd.DataFrame({'А4':[col1.number_input('Количество листов А4',0,100)],
-                    'A3':[col2.number_input('Количество листов А3',0,100)],
-                    'A2':[col3.number_input('Количество листов А2',0,100)],
-                    'A1':[col4.number_input('Количество листов А1',0,100)]
+df1 = pd.DataFrame({'А4':[col1.number_input('А4 Sheets',0,100)],
+                    'A3':[col2.number_input('А3 Sheets',0,100)],
+                    'A2':[col3.number_input('А2 Sheets',0,100)],
+                    'A1':[col4.number_input('А1 Sheets',0,100)]
                     })
 
-st.text('Проверьте введенные данные')
-df = pd.DataFrame({'Стаж':[experience],'Отдел':[department],"Сооружение":[building]})
+st.text('Check your data')
+df = pd.DataFrame({'Experience':[experience],'Department':[department],"Building":[building]})
 
 X_not_edited = pd.concat([df,df1], sort=False, axis =1)
 st.data_editor(df1,num_rows=1)
 st.data_editor(X_not_edited,num_rows=1)
 
-departmentID = pd.DataFrame({"Отдел":["АСО","ВиВ","ОВиК"],
-                             "Значение":[0.6,0.08,0.11]})
-buildingID = pd.DataFrame({"Сооружение":["Аэротенк","Камера сбора осадка","Буферный резервуар","Здание решеток","Воздуходувная станция","Отстойник","Резервуар-усреднитель","НСВИ","Цех обработки осадка","АБК","Мехочистка","Реагентное хозяйство ФК"],
-                           "Сложность":[.05,.01,.01,.1,.12,.02,.01,.07,.32,.1,.07,.1]})
+departmentID = pd.DataFrame({"Department":["АСО","ВиВ","ОВиК"],
+                             "Value":[0.6,0.08,0.11]})
+buildingID = pd.DataFrame({"Building":["Аэротенк","Камера сбора осадка","Буферный резервуар","Здание решеток","Воздуходувная станция","Отстойник","Резервуар-усреднитель","НСВИ","Цех обработки осадка","АБК","Мехочистка","Реагентное хозяйство ФК"],
+                           "Difficulty":[.05,.01,.01,.1,.12,.02,.01,.07,.32,.1,.07,.1]})
 
-dep = departmentID["Значение"].iloc[int(departmentID[departmentID["Отдел"]==department].index[0])]
+dep = departmentID["Value"].iloc[int(departmentID[departmentID["Department"]==department].index[0])]
 
-bui = buildingID["Сложность"].iloc[int(buildingID[buildingID["Сооружение"]==building].index[0])]
+bui = buildingID["Difficulty"].iloc[int(buildingID[buildingID["Building"]==building].index[0])]
 
 
-df_edited = pd.DataFrame({"Строение":[bui],'Отдел':[dep],'Стаж':[experience],})
+df_edited = pd.DataFrame({"Building":[bui],'Department':[dep],'Experience':[experience],})
 X_test = pd.concat([df_edited,df1], sort=False, axis =1)
 st.data_editor(X_test,num_rows=1)
 
@@ -47,14 +47,14 @@ model = pd.read_pickle('https://github.com/Malassalive/Bachelor-s-thesis/raw/mai
 st.divider()
 
 st.header('Теперь нажимайте на кнопку ниже!')
-st.text('Загляните в свое будущее')
-if (st.button('Узнать ответ')):
-    st.text('Полученные нами данные')
+st.text('A fortuneteller button')
+if (st.button('Answer')):
+    st.text('We recieved')
     st.dataframe(X_test)
     st.image('https://lh3.google.com/u/0/d/1vCK2EGGMMLtszAl7FpBJNbqaSEok0zDc=w1318-h644') 
     st.divider()
     answer = str(model.predict(X_test).round(0))
-    st.write('Проектирование приблизительно займет', answer, ' часа')
+    st.write('The projecting process will take approximately', answer, ' hours')
     st.header(answer)
     
     
